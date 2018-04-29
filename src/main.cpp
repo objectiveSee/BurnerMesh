b5f2744be037a435539702dfd7aab0a5c2fd1ba2
objectiveSee <dan.ricciotti@gmail.com>
2018-04-28 22:43:09 -0400

    delete unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,98 @@
-#include <Arduino.h>
-#include <painlessMesh.h>
-#include "network.h"
-#include "debug.h"
+#include "painlessMesh.h"
 
-// #define   MESH_PREFIX     "whateverYouLike"
-// #define   MESH_PASSWORD   "somethingSneaky"
-// #define   MESH_PORT       5555
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoPixel.h>
+#include "Adafruit_SSD1306.h"
 
-using namespace LightSwarm;
-Network         network;
+#include "display.h"
+#include "lights.h"
 
-// Scheduler userScheduler; // to control your personal task
-// painlessMesh  mesh;
 
-// // User stub
-// void sendMessage() ; // Prototype so PlatformIO doesn't complain
-//
-// Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
-//
-// void sendMessage() {
-//   Serial.println("Sending Message");
-//
-//   String msg = "Hello from node ";
-//   msg += mesh.getNodeId();
-//   mesh.sendBroadcast( msg );
-//   taskSendMessage.setInterval( random( TASK_SECOND * 5, TASK_SECOND * 10 ));
-// }
+#define   MESH_PREFIX     "FakeFrenchWifi"
+#define   MESH_PASSWORD   "ThisIsNotSecure"
+#define   MESH_PORT       5555
 
-// // Needed for painless library
-// void receivedCallback( uint32_t from, String &msg ) {
-//   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-// }
-//
-// void newConnectionCallback(uint32_t nodeId) {
-//     Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-// }
-//
-// void changedConnectionCallback() {
-//     Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
-// }
-//
-// void nodeTimeAdjustedCallback(int32_t offset) {
-//     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
-// }
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+void report();
+
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+Task taskReport( TASK_SECOND * 10 , TASK_FOREVER, &report );
+
+void report() {
+  std::list<uint32_t> m_nodes = mesh.getNodeList();
+  Serial.printf(" [NET] Connection list (num=%d):", m_nodes.size());
+  for (auto node : m_nodes) {
+      Serial.printf(" %x ", node);
+  }
+  Serial.print("\n");
+}
+
+void sendMessage() {
+  String msg = "Hello from node ";
+  msg += mesh.getNodeId();
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+    Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("Hello, World!\n");
+  Serial.begin(115200);
 
-    INFO("SETUP 1: NodeId=%08X\n", network.GetNodeID());
-    network.Init();
-    INFO("SETUP 2: NodeId=%08X\n", network.GetNodeID());
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-// mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  // mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
-  //
-  // mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
-  // mesh.onReceive(&receivedCallback);
-  // mesh.onNewConnection(&newConnectionCallback);
-  // mesh.onChangedConnections(&changedConnectionCallback);
-  // mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-  //
-  // userScheduler.addTask( taskSendMessage );
-  // taskSendMessage.enable();
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+
+  userScheduler.addTask( taskReport );
+  taskReport.enable();
+
+  displaySetup();
+
+  lights_setup();
 }
 
 void loop() {
-  // userScheduler.execute(); // it will run mesh scheduler as well
-  // mesh.update();
 
-  network.Update();
-  delay(100);
+  userScheduler.execute(); // it will run mesh scheduler as well
+  mesh.update();
+
+  static long last_display_update = 0;
+  static long last_known_node_count = -1;
+  if ( millis() - last_display_update > 1000 ) {
+    int node_count = mesh.getNodeList().size();
+    if ( node_count != last_known_node_count) {
+      Serial.print("Node count is now "); Serial.println(node_count);
+      last_known_node_count = node_count;
+    }
+    displayPrintStats(node_count);
+    last_display_update = millis();
+  }
+  lights_loop();
 }
