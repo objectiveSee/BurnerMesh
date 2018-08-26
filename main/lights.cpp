9b3768bd4f8d4d270c7f6d1234f2efed45d0b877
objectiveSee <dan.ricciotti@gmail.com>
2018-08-26 12:49:49 -0700

    add 3 light patterns

--- main/lights.cpp
+++ main/lights.cpp
@@ -17,10 +17,16 @@ CRGB leds[NUM_LEDS];
 CRGB wheel(int WheelPos, int dim);
 Network *myNetwork;
 
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+
 void setPixelHeatColor (int Pixel, byte temperature);
 void Fire(int Cooling, int Sparking);
 void TwinkleRandom();
 void Rainbow(byte, int, int);
+void plasma();
+void blendwave();
+void sinelon();
 
 /**
  * Light modes as enum.
@@ -83,13 +89,15 @@ void allColor(CRGB color) {
 
 void lights_loop() {
 
-  switch (_lightMode) {
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+
+  switch(_lightMode) {
     case LIGHT_MODE_RED: {
         allColor(CRGB(255,0,0));
     }
     break;
     case LIGHT_MODE_PURPLE: {
-      allColor(0xFF3060);
+      allColor(0x5D2275);
     }
     break;
     case LIGHT_MODE_GREEN: {
@@ -111,6 +119,22 @@ void lights_loop() {
       Rainbow(10,30,100);
     }
     break;
+    
+    case LIGHT_MODE_PLASMA: {
+      plasma();
+    }
+    break;
+
+    case LIGHT_MODE_BLENDWAVE: {
+      blendwave();
+    }
+    break;
+ 
+    case LIGHT_MODE_SINELON: {
+      sinelon();
+    }
+    break;
+    
     default:{
       
     }
@@ -269,9 +293,62 @@ void Rainbow(byte nthLedOff, int rainbowSpeed, int pixelSpeed) {
   fill_rainbow( leds, NUM_LEDS, pos, 1);
 
   for (int i = 0; i < NUM_LEDS; i++ ) {
-    if ( i%nthLedOff == offPos ) {
+      if ( i%nthLedOff == offPos ) {
       leds[i] = CRGB::Black;
     }
   }
 }
 
+
+// Use qsuba for smooth pixel colouring and qsubd for non-smooth pixel colouring
+#define qsubd(x, b)  ((x>b)?b:0)                              // Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
+#define qsuba(x, b)  ((x>b)?x-b:0)                            // Analog Unsigned subtraction macro. if result <0, then => 0
+
+CRGBPalette16 currentPalette = PartyColors_p;
+TBlendType    currentBlending = LINEARBLEND;                  // NOBLEND or LINEARBLEND
+
+void plasma() {                                                 // This is the heart of this program. Sure is short. . . and fast.
+
+  int thisPhase = beatsin8(6,-64,64);                           // Setting phase change for a couple of waves.
+  int thatPhase = beatsin8(7,-64,64);
+
+  for (int k=0; k<NUM_LEDS; k++) {                              // For each of the LED's in the strand, set a brightness based on a wave as follows:
+
+    int colorIndex = cubicwave8((k*23)+thisPhase)/2 + cos8((k*15)+thatPhase)/2;           // Create a wave and add a phase change and add another wave with its own phase change.. Hey, you can even change the frequencies if you wish.
+    int thisBright = qsuba(colorIndex, beatsin8(7,0,96));                                 // qsub gives it a bit of 'black' dead space by setting sets a minimum value. If colorIndex < current value of beatsin8(), then bright = 0. Otherwise, bright = colorIndex..
+
+    leds[k] = ColorFromPalette(currentPalette, colorIndex, thisBright, currentBlending);  // Let's now add the foreground colour.
+  }
+}
+
+void blendwave() {
+
+  CRGB clr1;
+  CRGB clr2;
+  uint8_t speed;
+  uint8_t loc1;
+  uint8_t loc2;
+  uint8_t ran1;
+  uint8_t ran2;
+
+  speed = beatsin8(6,0,255);
+
+  #define BLENDYBRIGHT 128
+
+  clr1 = blend(CHSV(beatsin8(3,0,255),255,BLENDYBRIGHT), CHSV(beatsin8(4,0,255),255,BLENDYBRIGHT), speed);
+  clr2 = blend(CHSV(beatsin8(4,0,255),255,BLENDYBRIGHT), CHSV(beatsin8(3,0,255),255,BLENDYBRIGHT), speed);
+
+  loc1 = beatsin8(10,0,NUM_LEDS-1);
+  
+  fill_gradient_RGB(leds, 0, clr2, loc1, clr1);
+  fill_gradient_RGB(leds, loc1, clr2, NUM_LEDS-1, clr1);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+  
